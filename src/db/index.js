ce402eab2ad951b5823bb0c942ba239b3b729a25
Shaun Wassell <shaun@trove.ai>
2021-08-08 23:12:12 -0400

    02.06 - Added route for adding meals

--- src/db/index.js
+++ src/db/index.js
@@ -4,3 +4,5 @@ export { deleteMeal } from './deleteMeal';
 export { getIngredients } from './getIngredients';
 export { getPopulatedMeals } from './getPopulatedMeals';
 export { insertIngredient } from './insertIngredient';
+export { insertMeal } from './insertMeal';
+export { searchRecipes } from './searchRecipes';
